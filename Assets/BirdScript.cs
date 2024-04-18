084c31e2ec85a61bb7a2a9e16069e02677b141ec
Ricardo Rosario <ricardorosariorr4@gmail.com>
2024-04-18 11:20:31 -0100

    ci integration with GameCI

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,10 +7,6 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public float flapStrength;
     // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
